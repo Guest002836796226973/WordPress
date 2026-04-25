319c401fce278ac85513e8180dd581d49f0df775
Petitpoids <103829649+Guest002836796226973@users.noreply.github.com>
2026-04-25 19:52:14 +0200

    Update vanilla_lazyload_mode.js

--- Script/vanilla_lazyload_mode.js
+++ Script/vanilla_lazyload_mode.js
@@ -2,6 +2,8 @@ function vanilla_x_lazyload(global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.LazyLoad = factory());
+  vanilla_x_lazyload.init();
+  return;
 }
 
 (function() { 'use strict';
