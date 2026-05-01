b057d4635ccf2166819a8b2cbdba09e113e548c3
Petitpoids <103829649+Guest002836796226973@users.noreply.github.com>
2026-05-01 21:18:24 +0200

    Update pixel.js

--- Script/pixel.js
+++ Script/pixel.js
@@ -1,4 +1,7 @@
+// Le mode multi-selectors à un meilleur retour ?!
 const WH = document.querySelectorAll("html");
+const WH = document.querySelectorAll("iframe");
+const WH = document.querySelectorAll("widget");
 function WS() {
     WH.intElemClientWidth = element.clientWidth;
     WH.intElemClientHeight = element.clientHeight;
@@ -14,32 +17,4 @@ window.addEventListener("resize", WS);
 window.onresize = WS;
 // window.onresize = function(){ location.reload(WS); }
 
-const WHIf = document.querySelectorAll("iframe");
-function WSIf() {
-    WHIf.intElemClientWidth = element.clientWidth;
-    WHIf.intElemClientHeight = element.clientHeight;
-    WHIf.intElemOffsetWidth = element.offsetWidth;
-    WHIf.intElemOffsetHeight = element.offsetHeight;
-    WHIf.Width = window.innerWidth;
-    WHIf.Height = window.innerHeight;
-    WHIf.textContent = window.innerWidth;
-    WHIf.textContent = window.innerHeight;
-}
-WSIf();
-window.addEventListener("resize", WSIf);
-window.onresize = WSIf;
-
-const WHWi = document.querySelectorAll("widget");
-function WSWi() {
-    WHWi.intElemClientWidth = element.clientWidth;
-    WHWi.intElemClientHeight = element.clientHeight;
-    WHWi.intElemOffsetWidth = element.offsetWidth;
-    WHWi.intElemOffsetHeight = element.offsetHeight;
-    WHWi.Width = window.innerWidth;
-    WHWi.Height = window.innerHeight;
-    WHWi.textContent = window.innerWidth;
-    WHWi.textContent = window.innerHeight;
-}
-WSWi();
-window.addEventListener("resize", WSWi);
-window.onresize = WSWi;
+// Fractionner le code ci-avant selon votre expérience
