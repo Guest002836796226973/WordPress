552fa0bfaa7b61fa96afb997201d46405ee1e0f0
Petitpoids <103829649+Guest002836796226973@users.noreply.github.com>
2026-05-01 20:50:03 +0200

    Update pixel.js

--- Script/pixel.js
+++ Script/pixel.js
@@ -1,8 +1,23 @@
 const WH = document.querySelectorAll("#pixel");
 function WHSize() {
-  WH.Width = window.innerWidth;
-  WH.Height = window.innerHeight;
+    WH.intElemClientWidth = element.clientWidth;
+    WH.intElemClientHeight = element.clientHeight;
+    WH.intElemOffsetWidth = element.offsetWidth;
+    WH.intElemOffsetHeight = element.offsetHeight;
+    WH.Width = window.innerWidth;
+    WH.Height = window.innerHeight;
+    WH.textContent = window.innerWidth;
+    WH.textContent = window.innerHeight;
 }
 WHSize();
 window.addEventListener("resize", WHSize);
 window.onresize = WHSize;
+// window.onresize = function(){ location.reload(WHSize); } // Recharger la page lors du redimensionnement
+
+CSS.escape("html");
+CSS.escape("widget");
+CSS.escape("iframe");
+CSS.escape(".foo#bar");
+CSS.escape("()[]{}");
+CSS.escape(0);
+CSS.escape('\0');
