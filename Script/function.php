c3cbd1e53dc7001bef5282e02aad158e33c2e08f
Petitpoids <103829649+Guest002836796226973@users.noreply.github.com>
2026-05-12 00:33:23 +0200

    Update function.php

--- Script/function.php
+++ Script/function.php
@@ -18,8 +18,6 @@ add_filter('embed_oembed_html', 'remplace_youtube', 10, 6);
 function remplace_youtube( $html, $cached_html, $url, $content, $post_id, $attr) {
     $i = 1;
     do {
-	$i;
-	// $i++; // compte à rebours;
     $fragmenter = parse_url($url);
 	$you_t = preg_match('/^((m|www)\.)?youtube\.com|youtu\.be$/i', $fragmenter['host']);
 	if ( !$you_t ) {
@@ -42,6 +40,8 @@ function remplace_youtube( $html, $cached_html, $url, $content, $post_id, $attr)
 	} else if ( str_contains($attr, $replace) ) {
        $attr = str_replace( $replace, 'youtube-nocookie.com', $attr);
 	}
+	$i;
+	// $i++; // compte à rebours;
     }
 	while ($i < 2); // tourne en boucle
 }
