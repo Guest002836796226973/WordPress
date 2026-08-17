da789a4a309593e8acb61ca333c19645c4db3817
Petitpoids <103829649+Guest002836796226973@users.noreply.github.com>
2026-08-17 21:27:50 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -5,16 +5,41 @@ header('Content-Type: text/html; charset=utf-8');
 header('Surrogate-Control: BigPipe/1.0');
 header('X-Accel-Buffering: no');
 
+
 ini_set('session.cookie_secure', 'On');
-// session_start(); // Démarrage intempestif de la session "ex:chat"
+
 session_unset();
 session_destroy();
 session_write_close();
 
 unset($_COOKIE[$key]);
-setcookie(session_name(),'',86400,'/','monsite.fr'); // nom du domaine
-session_regenerate_id(true);
-session_set_cookie_params(['lifetime' => 86400, 'secure' => true, 'samesite' => 'strict']);
+setcookie(session_name(),'',0,'/','monsite.fr');
+session_set_cookie_params(['lifetime' => 0, 'secure' => true, 'samesite' => 'strict']);
+
+if (isset($_COOKIE['session_name'])) {
+            ini_set("session.use_trans_sid",false);
+            session_start();
+            $_SESSION['cookie_support']=1;
+} else {
+            ini_set("session.use_trans_sid",true);
+            session_start();
+            $_SESSION['cookie_support']=0;
+} 
+
+if (isset($_SESSION['destroyed'])
+    && $_SESSION['destroyed'] < time() - 300) {
+    remove_all_authentication_flag_from_active_sessions($_SESSION['userid']);
+    throw(new DestroyedSessionAccessException);
+}
+
+$old_sessionid = session_id();
+$_SESSION['destroyed'] = time();
+
+session_regenerate_id();
+
+unset($_SESSION['destroyed']);
+
+$new_sessionid = session_id();
 
 // url site
 define('WP_SITEURL', 'https://monsite.fr');
